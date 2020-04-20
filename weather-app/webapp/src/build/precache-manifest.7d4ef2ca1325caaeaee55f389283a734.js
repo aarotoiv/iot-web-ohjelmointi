8cd21ea0ae8625bb2176bd995765cd74a138520c
aarotoiv <aaro.toivonen@gmail.com>
2020-04-20 11:27:51 +0300

    jk added it just now

---
+++ weather-app/webapp/src/build/precache-manifest.7d4ef2ca1325caaeaee55f389283a734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835ca4644f5eb73ff1ac9d5f50a69603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "627d9480f91464ac8b6a",
+    "url": "/static/css/main.037ee265.chunk.css"
+  },
+  {
+    "revision": "6399e1de09174cd4b569",
+    "url": "/static/js/2.ca5bb5d9.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.ca5bb5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627d9480f91464ac8b6a",
+    "url": "/static/js/main.b8efe244.chunk.js"
+  },
+  {
+    "revision": "a5c1561115dc7b0b49ac",
+    "url": "/static/js/runtime-main.ab8bee8b.js"
+  }
+]);
